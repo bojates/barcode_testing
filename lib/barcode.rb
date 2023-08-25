14d84aeb8229b71324e8a750424dd981dedafeaa
Jemima Kingsley <jemima@bojates.com>
2023-08-25 12:40:24 +0100

    Refactor Barcode to remove is_valid?

--- lib/barcode.rb
+++ lib/barcode.rb
@@ -1,10 +1,6 @@
 module Barcode
-  def self.is_valid?(barcode)
-    barcode.size == 10 || barcode.size == 13
-  end
-
   def self.verify_format(barcode)
-    if self.is_valid?(barcode)
+    if barcode.size == 10 || barcode.size == 13
       true
     else
       raise ArgumentError, "Invalid input"
