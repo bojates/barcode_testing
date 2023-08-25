63d36fccc4320472f0c8fdc3e6012516bd965267
Jemima Kingsley <jemima@bojates.com>
2023-08-25 12:13:47 +0100

    Explore a verify_format method

--- lib/barcode.rb
+++ lib/barcode.rb
@@ -2,4 +2,8 @@ module Barcode
   def self.is_valid?(barcode)
     barcode.size == 10 || barcode.size == 13
   end
+
+  def self.verify_format(barcode)
+    self.is_valid?(barcode)
+  end
 end
--- spec/barcode_spec.rb
+++ spec/barcode_spec.rb
@@ -2,29 +2,37 @@ require 'barcode'
 
 RSpec.describe Barcode do 
 
-  it 'returns a result when asked for barcode validity' do 
-    expect(Barcode::is_valid?('123')).to_not be_nil
+  describe 'is_valid?' do 
+    it 'returns a result when asked for barcode validity' do 
+      expect(Barcode::is_valid?('123')).to_not be_nil
+    end
+
+    it 'returns false if a barcode is less than 10 chars' do 
+      expect(Barcode::is_valid?('a' * (1..9).to_a.sample)).to be false
+    end
+
+    it 'returns false if a barcode is 11 or 12 chars' do 
+      expect(Barcode::is_valid?('1' * 11)).to be false
+      expect(Barcode::is_valid?('A' * 12)).to be false
+    end
+
+    it 'returns false if a barcode is more than 13 chars' do 
+      expect(Barcode::is_valid?('1' * 14)).to be false
+      expect(Barcode::is_valid?('H' * 200)).to be false
+    end
+
+    it 'returns true if a barcode has 10 chars' do 
+      expect(Barcode.is_valid?('0123456789')).to be true
+    end
+
+    it 'returns true if a barcode has 13 chars' do 
+      expect(Barcode.is_valid?('0123456789123')).to be true
+    end
   end
 
-  it 'returns false if a barcode is less than 10 chars' do 
-    expect(Barcode::is_valid?('a' * (1..9).to_a.sample)).to be false
-  end
-
-  it 'returns false if a barcode is 11 or 12 chars' do 
-    expect(Barcode::is_valid?('1' * 11)).to be false
-    expect(Barcode::is_valid?('A' * 12)).to be false
-  end
-
-  it 'returns false if a barcode is more than 13 chars' do 
-    expect(Barcode::is_valid?('1' * 14)).to be false
-    expect(Barcode::is_valid?('H' * 200)).to be false
-  end
-
-  it 'returns true if a barcode has 10 chars' do 
-    expect(Barcode.is_valid?('0123456789')).to be true
-  end
-
-  it 'returns true if a barcode has 13 chars' do 
-    expect(Barcode.is_valid?('0123456789123')).to be true
+  describe 'verify_format' do 
+    it 'returns true if passed a 10 digit string' do 
+      expect(Barcode::verify_format('0123456789')).to be true
+    end
   end
 end
